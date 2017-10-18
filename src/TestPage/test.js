330cc5edd8f40111a5b09c1ac1bbd12c2997646c
CEREBR4L <mattmillergraphics@gmail.com>
2017-10-18 21:31:46 +0100

    Routing implimented

---
+++ src/TestPage/test.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './test.css';
+
+class Test extends Component {
+  render() {
+    return (
+      <div className="test">
+        <h1>Welcome to Test Page</h1>
+      </div>
+    );
+  }
+}
+
+export default Test;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './base.css';
 import Home from './Home/home';
+import Test from './TestPage/test';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory'
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+const customHistory = createBrowserHistory()
+
+ReactDOM.render(
+    <Router history={customHistory}>
+      <div>
+        <nav className="nav">
+            <ul className="nav-list">
+                <li><NavLink exact activeClassName="active-nav-itm" className="nav-list-itm" to="/">Home</NavLink></li>
+                <li><NavLink exact activeClassName="active-nav-itm" className="nav-list-itm" to="/test">Test</NavLink></li>
+            </ul>
+        </nav>
+        
+        <div>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/test" component={Test}/>
+        </div>
+      </div>
+  </Router>
+
+
+    
+    , document.getElementById('root'),);
 registerServiceWorker();
