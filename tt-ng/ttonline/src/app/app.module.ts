bb00b41cd0a21c04d69557d4f61ece182dfb22f1
CEREBR4L <mattmillergraphics@gmail.com>
2018-01-25 19:57:26 +0000

    Restructure & Routing

--- tt-ng/ttonline/src/app/app.module.ts
+++ tt-ng/ttonline/src/app/app.module.ts
@@ -1,22 +1,62 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
-import { SocialStatsComponent } from './social-stats/social-stats.component';
-import { ImageWallComponent } from './image-wall/image-wall.component';
-import { ProjectWallComponent } from './project-wall/project-wall.component';
+import { SocialStatsComponent } from './Generic_Components/social-stats/social-stats.component';
+import { ImageWallComponent } from './Generic_Components/image-wall/image-wall.component';
+import { ProjectWallComponent } from './Generic_Components/project-wall/project-wall.component';
+import { Route } from '@angular/router/src/config';
+import { HomeComponent } from './Page_Components/home/home.component';
+import { ProjectsComponent } from './Page_Components/projects/projects.component';
+import { WorkComponent } from './Page_Components/work/work.component';
+import { AboutComponent } from './Page_Components/about/about.component';
+import { FourOhFourComponent } from './Page_Components/four-oh-four/four-oh-four.component';
+import { NavComponent } from './Generic_Components/nav/nav.component';
+import { FooterComponent } from './Generic_Components/footer/footer.component';
 
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'work',
+    component: WorkComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '**',
+    component: FourOhFourComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     SocialStatsComponent,
     ImageWallComponent,
-    ProjectWallComponent
+    ProjectWallComponent,
+    HomeComponent,
+    ProjectsComponent,
+    WorkComponent,
+    AboutComponent,
+    FourOhFourComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
